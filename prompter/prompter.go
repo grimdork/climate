176deb523c9693a4e26c568f23b37c18752ea8a1
Ronny Bangsund <github@grimdork.net>
2022-11-30 13:50:28 +0100

    Added question prompter.

---
+++ prompter/prompter.go
@@ -0,0 +1,71 @@
+package prompter
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/term"
+)
+
+// Prompter is the interface for prompting the user for input.
+type Prompter struct {
+	*bufio.Reader
+	// Questions to ask.
+	Questions []string
+	// Secret sets the corresponding question to be a secret, hiding input.
+	Secret []bool
+	// Answers from the user.
+	Answers []string
+}
+
+// Question is a question to ask the user.
+type Question struct {
+	// Question to present. Include a question mark if necessary.
+	Question string
+	// Secret sets the question to be a secret, hiding input.
+	Secret bool
+}
+
+// New creates a new Prompter.
+func New(q []Question) *Prompter {
+	pr := &Prompter{}
+	pr.Reader = bufio.NewReader(os.Stdin)
+	for _, q := range q {
+		pr.Questions = append(pr.Questions, q.Question)
+		pr.Secret = append(pr.Secret, q.Secret)
+	}
+
+	return pr
+}
+
+// Ask the user for input.
+func (pr *Prompter) Ask() error {
+	pr.Answers = make([]string, len(pr.Questions))
+	var t string
+	var err error
+	for i, q := range pr.Questions {
+		fmt.Printf("%s: ", q)
+		if pr.Secret[i] {
+			sec, err := term.ReadPassword(int(syscall.Stdin))
+			println()
+			if err != nil {
+				return err
+			}
+
+			t = string(sec)
+		} else {
+			t, err = pr.ReadString('\n')
+			if err != nil {
+				return err
+			}
+		}
+
+		t = strings.ToLower(strings.TrimSpace(t))
+		pr.Answers[i] = t
+	}
+
+	return nil
+}
