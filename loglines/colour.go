f49ca2b2f4b0c76fb34fde779619ac30f7238882
Urethramancer <github@grimdork.net>
2026-03-11 05:31:31 +0100

    Hopefully fixed th order of things.

--- loglines/colour.go
+++ loglines/colour.go
@@ -1,16 +1,16 @@
 package loglines
 
 import (
-	"fmt"
 	"github.com/grimdork/climate/cfmt"
 	"github.com/grimdork/climate/str"
 )
 
 // CMsg prints a colourised message to stdout. Timestamp is grey; message text is cyan by default.
+// It respects colour tags in the format string and supports fmt verbs by using cfmt.Sprintf.
 func CMsg(f string, v ...any) {
 	b := str.NewStringer()
 	b.WriteStrings(NowString(), ": ")
-	msg := fmt.Sprintf(f, v...)
+	msg := cfmt.Sprintf(f, v...)
 	// Use %grey for timestamp and %cyan for message body, and reset at the end.
 	cfmt.Println("%grey" + b.String() + "%reset %cyan" + msg + "%reset")
 }
