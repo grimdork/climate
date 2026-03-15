ae432120ab5acdb520fba6bc00be369612c9737e
Urethramancer <github@grimdork.net>
2026-03-15 02:52:27 +0100

    Added manual test for help print debugging.

---
+++ arg/help_manual_test.go
@@ -0,0 +1,15 @@
+package arg_test
+
+import (
+	"testing"
+
+	"github.com/grimdork/climate/arg"
+)
+
+func TestPrintHelpShowsChoices(t *testing.T) {
+	opt := arg.New("testapp")
+	opt.SetDefaultHelp(true)
+	op := []any{"dev", "prod", "test"}
+	opt.SetOption(arg.GroupDefault, "m", "mode", "Run mode.", "dev", false, arg.VarString, op)
+	opt.PrintHelp()
+}
