bae5f79172f3937e285c5d571dcdc869cce74d0b
Urethramancer <github@grimdork.net>
2026-03-20 02:57:35 +0100

    arg: New(name,desc) API; show appdesc inline; update callers and fix mangled errors.New

--- arg/errors.go
+++ arg/errors.go
@@ -4,30 +4,30 @@ import "errors"
 
 var (
 	// ErrRunCommand is not really an error, but indicates a command was run.
-	ErrRunCommand = errors.New("command run", "")
+	ErrRunCommand = errors.New("command run")
 	// ErrHandled indicates a command printed its own help or handled the error and the
 	// top-level should not print its own help (non-zero exit expected).
-	ErrHandled = errors.New("handled by command", "")
+	ErrHandled = errors.New("handled by command")
 	// ErrNoArgs is returned when no arguments are provided.
-	ErrNoArgs = errors.New("no arguments provided", "")
+	ErrNoArgs = errors.New("no arguments provided")
 	// ErrIllegalChoice is returned when the argument isn't in the approved list of choices.
-	ErrIllegalChoice = errors.New("illegal choice", "")
+	ErrIllegalChoice = errors.New("illegal choice")
 	// ErrMissingRequired is returned when a required option is missing.
-	ErrMissingRequired = errors.New("missing required option", "")
+	ErrMissingRequired = errors.New("missing required option")
 	// ErrMissingParam is returned when an option is missing an argument.
-	ErrMissingParam = errors.New("missing option parameter", "")
+	ErrMissingParam = errors.New("missing option parameter")
 	// ErrMissingFunc is returned when a command is missing a function.
-	ErrMissingFunc = errors.New("missing function", "")
+	ErrMissingFunc = errors.New("missing function")
 	// ErrLongShort is returned when a short option is longer than one character.
-	ErrLongShort = errors.New("short option must be one character", "")
+	ErrLongShort = errors.New("short option must be one character")
 	// ErrUnknownOption is returned when an undefined option is encountered.
-	ErrUnknownOption = errors.New("unknown option", "")
+	ErrUnknownOption = errors.New("unknown option")
 	// ErrEmptyLong is returned when a long option is empty.
-	ErrEmptyLong = errors.New("long option without a string", "")
+	ErrEmptyLong = errors.New("long option without a string")
 	// ErrShortLong is returned when a long option is shorter than two characters.
-	ErrShortLong = errors.New("long option must be at least two characters", "")
+	ErrShortLong = errors.New("long option must be at least two characters")
 	// ErrUnknownType is returned when an unknown option variable type is encountered.
-	ErrUnknownType = errors.New("unknown option type", "")
+	ErrUnknownType = errors.New("unknown option type")
 	// ErrNoPlaceholder is returned when a positional argument is missing a placeholder.
-	ErrNoPlaceholder = errors.New("no placeholder", "")
+	ErrNoPlaceholder = errors.New("no placeholder")
 )
